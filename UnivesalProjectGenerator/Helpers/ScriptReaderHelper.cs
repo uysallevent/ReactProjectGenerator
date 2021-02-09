2dd98711a1ec9d352cd0bbdf0aef3cc185514e5f
levent.uysal <levent.uysal@algomedi.com>
2021-02-09 15:27:15 +0300

    Complete Cli

---
+++ UnivesalProjectGenerator/Helpers/ScriptReaderHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnivesalProjectGenerator.Enums;
+
+namespace UniversalProjectGenerator.Helpers
+{
+    public static class ScriptReaderHelper
+    {
+
+        public static string ReadScriptFile(string scriptPath)
+        {
+            if (!File.Exists(scriptPath))
+            {
+                throw new FileNotFoundException($"There is no file on {scriptPath} path");
+            }
+            var content = File.ReadAllText(scriptPath);
+            var script = InputChecker(content);
+            return script;
+        }
+
+        private static string InputChecker(string content)
+        {
+            if (content.Contains(":{") || content.Contains("}:"))
+            {
+                var countInput = content.Split(":{").Length - 1;
+                for (int i = 0; i < countInput; i++)
+                {
+                    var schemaName = content.Substring(content.IndexOf(":{"), (content.IndexOf("}:") - content.IndexOf(":{")) + 2);
+                    while (true)
+                    {
+                        Console.WriteLine($"Please enter a {schemaName.Replace(":{", "").Replace("}:", "")} :");
+                        var inputValue = Console.ReadLine();
+                        if (!string.IsNullOrEmpty(inputValue))
+                        {
+                            content = content.Replace(schemaName, inputValue);
+                            break;
+                        }
+                        else
+                        {
+                            Console.SetCursorPosition($"Please enter a {schemaName.Replace(":{", "").Replace("}:", "")} :".Length, 0);
+                        }
+                    }
+
+                }
+                return content;
+            }
+            return null;
+        }
+    }
+}
---
+++ UnivesalProjectGenerator/Program.cs
@@ -0,0 +1,81 @@
+﻿using UniversalProjectGenerator.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management.Automation.Runspaces;
+using System.Threading.Tasks;
+using UnivesalProjectGenerator.ProjectTypes.React;
+
+namespace UniversalProjectGenerator
+{
+    static class Program
+    {
+        static string projectPath;
+        static StringArrayComparer stringArrayComparer;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                stringArrayComparer = new StringArrayComparer();
+                MainAsync(args).Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            await Console.Out.WriteLineAsync(" - - - - - - - - - - - - - - - - - - ");
+            await Console.Out.WriteLineAsync("Algomedi Universal Project Generator");
+            await Console.Out.WriteLineAsync(" - - - - - - - - - - - - - - - - - - ");
+            await Console.Out.WriteLineAsync();
+
+
+            await WaitCommandAsync();
+        }
+
+        private static async Task WaitCommandAsync()
+        {
+            while (true)
+            {
+                await Console.Out.WriteLineAsync("Waiting your command :").ConfigureAwait(false);
+                await ExecuteCommandAsync(Console.ReadLine().Split(' ').Select(p => p.ToLowerInvariant()).ToArray());
+            }
+        }
+
+        private static async Task ExecuteCommandAsync(string[] command)
+        {
+            foreach (var item in GeneralCommandList())
+            {
+                if (stringArrayComparer.Equals(item.Key, command))
+                {
+                    await item.Value.Invoke("testtt");
+                    return;
+                }
+            }
+            await Console.Out.WriteLineAsync($"There is no command found as {string.Join(" ", command)}");
+        }
+
+        private static Dictionary<string[], Func<string, Task>> GeneralCommandList()
+        {
+            return new Dictionary<string[], Func<string, Task>>()
+            {
+                 { new string[]{"help"}, async (c) =>{ await Console.Out.WriteLineAsync("Help contents");}},
+                 { new string[]{"react", "generate" }, async (c) =>{await ReactGeneration.Generate(); }},
+                 { new string[]{ "react", "test" }, async (c) =>{ await Console.Out.WriteLineAsync("Test2 ignated"); }}
+            };
+        }
+
+
+
+
+
+
+
+    }
+}
